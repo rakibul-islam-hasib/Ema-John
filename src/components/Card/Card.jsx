4f44462f06e3663dcf6b5cb8551d79a3b8effb51
Hasib Hossain <boraborhasib@gmail.com>
2023-03-27 17:52:36 +0600

    Fp

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 
-const Card = ({ props }) => {
-    console.log(props)
-    const { img, name, seller, ratings , price} = props;
+const Card = (props) => {
+    // console.log(props)
+   const handelCart = props.handelCart;
+    const { img, name, seller, ratings , price , id} = props.props;
     return (
         <>
-            <div className=" h-[530px] relative rounded-lg bg-base-100 border">
-                <figure className='px-4 py-4'><img className='rounded-lg' src={img} alt="Shoes" /></figure>
+            <div className=" h-[450px] w-[300px] relative rounded-lg bg-base-100 border">
+                <figure className='px-4 py-4'><img className='rounded-lg w-full h-[200px]' src={img} alt="Shoes" /></figure>
                 <div className="px-4 py-4">
                     <h2 className="card-title">{name}</h2>
                     <h4 className='text-2xl mb-3'>Price : {price}$</h4>
                     <p>Manufacturer : {seller}</p>
                     <p>Rating : {ratings} Stars</p>
                 </div>
-                    <button className='w-[100%] hover:bg-orange-300 transition-[2s] absolute bottom-0 rounded-b-lg py-2 mt-3 bg-[#FFE0B3]'>Add To Cart</button>
+                    <button onClick={() => handelCart(props)} className='w-[100%] hover:bg-orange-300 transition-[2s] absolute bottom-0 rounded-b-lg py-2 mt-3 bg-[#FFE0B3]'>Add To Cart</button>
             </div>
         </>
     );
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -7,17 +7,23 @@ const Shop = () => {
         fetch('products.json')
         .then(res=>res.json())
         .then(data => setProducts(data))
-    }, [])
+    }, []); 
+    const [cart , setCart] = useState([]); 
+    const handelCart = (id) => {
+        const neCart = [...cart , id] ; 
+        setCart(neCart)
+    }
     return (
         <div>
             <div className="ml-4 grid mt-5 grid-cols-5">
                 <div className="col-span-4 grid grid-cols-3 gap-6">
                     {
-                        products.map(product => <Card key={product.id} props={product}></Card>)
+                        products.map(product => <Card key={product.id} handelCart={handelCart} props={product}></Card>)
                     }
                 </div>
                 <div className="col-span-1">
-                    <h1 className='text-right'>Order Summary</h1>
+                    <h1 className='text-center text-2xl font-semibold'>Order Summary</h1>
+                    <p>Selected Items : {cart.length}</p>
                 </div>
             </div>
         </div>
