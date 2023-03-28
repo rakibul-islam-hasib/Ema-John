82178dbe34d37efa111e67356a19a9abd8ff443b
Hasib Hossain <boraborhasib@gmail.com>
2023-03-28 17:12:16 +0600

    Quantity Bug Fixed

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import { addCommas } from '../Utilities/NumberWithComma';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrashCan , faArrowRight} from '@fortawesome/free-solid-svg-icons'; 
+import { faTrashCan, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 let trash = <FontAwesomeIcon icon={faTrashCan} />
 let arrow = <FontAwesomeIcon icon={faArrowRight} />
 const Cart = ({ cart }) => {
-    
+
     let totalPrice = 0;
-    let totalShipping = 0 ; 
+    let totalShipping = 0;
+    let quantity = 0;
     for (const product of cart) {
+        if (product.quantity === 0) {
+            product.quantity = 1;
+        }
         // product.quantity = product.quantity || 1 ; 
-        totalPrice = totalPrice + product?.props?.price  || product.price; 
-        totalShipping = totalShipping + product?.props?.shipping || product.price; 
+        totalPrice = totalPrice + product?.props?.price || product.price;
+        totalShipping = totalShipping + product?.props?.shipping || product.price;
+        quantity = quantity + product?.quantity || product?.props?.quantity;
         console.log(product.quantity)
     }
-    let tax = totalPrice*2/100 ; 
-    let grandTotal = totalPrice + totalShipping + tax ; 
+    let tax = totalPrice * 2 / 100;
+    let grandTotal = totalPrice + totalShipping + tax;
     return (
         <div className='bg-[#ff99004d] sticky top-16 px-7 py-7 h-[100vh]'>
             <h1 className='text-center text-2xl font-semibold'>Order Summary</h1>
-            <p className='my-1'>Selected Items : {cart.length}</p>
+            <p className='my-1'>Selected Items : {quantity}</p>
             <p>Total Price : {addCommas(totalPrice)}$</p>
             <p className='my-1'>Total Shipping : {addCommas(totalShipping)}$</p>
             <p className='mb-1'>Tax : {tax}$</p>
